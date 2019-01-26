aed6d59397f971e147c2ecb2a419ab4c0e835ed5
Chisanga Louis Siwale <chisanga.siwale@gmail.com>
2019-01-26 09:06:50 +0200

    .gitignore fix

--- static/MFL/js/main.js
+++ static/MFL/js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    $('.nav.navbar-nav > li').on('click', function (e) {
-        e.preventDefault();
-        $('.nav.navbar-nav > li').removeClass('active');
-        $(this).addClass('active');
-    });
-
     $('#search-toggle-btn').on('click', function () {
         $('#sidebar').toggleClass('active');
         $("#search-toggle-btn").toggleClass("fa-chevron-circle-left");
